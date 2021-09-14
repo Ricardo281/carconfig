195b7f5d68db9cb44a2cd8e5fe7bc2d4d7382846
Ricardo281 <ricardo.landolt@stud.hslu.ch>
2021-09-13 23:22:21 -1200

    updated connectodb function, to avoid max_requests_per_hour error

--- R/connectToDb_func.r
+++ R/connectToDb_func.r
@@ -1,7 +1,7 @@
 #' A Connection to the Database
 #'
-#' This function allows you to connect to the MySql Database. 
-#' @return connectToDb_func() returns an S4 object that inherits from DBIConnection. 
+#' This function allows you to connect to the MySql Database.
+#' @return connectToDb_func() returns an S4 object that inherits from DBIConnection.
 #'    This object is used to communicate with the database engine.
 #' @export
 #' @examples
@@ -10,14 +10,27 @@
 #' sqlstring <- "SELECT * FROM ConfigCars"
 #' df <- dbGetQuery(con, sqlstring)
 
+
 connectToDb_func <- function(){
-  con <- RMySQL::dbConnect(
-    RMySQL::MySQL(),
-    user = "u723490217_ricardo",
-    password = "o.JphvmhZNACmm8gXJbiAKuLp2YCrU3z5P",
-    dbname = "u723490217_Astra",
-    host = "185.224.138.133"
-  )
+  if(length(DBI::dbListConnections(RMySQL::MySQL())) < 16) {
+    con <- RMySQL::dbConnect(
+      RMySQL::MySQL(),
+      user = "u723490217_ricardo",
+      password = "o.JphvmhZNACmm8gXJbiAKuLp2YCrU3z5P",
+      dbname = "u723490217_Astra",
+      host = "185.224.138.133"
+    )
+  } else {
+    lapply(DBI::dbListConnections(RMySQL::MySQL()), DBI::dbDisconnect)
+    con <- RMySQL::dbConnect(
+      RMySQL::MySQL(),
+      user = "u723490217_ricardo",
+      password = "o.JphvmhZNACmm8gXJbiAKuLp2YCrU3z5P",
+      dbname = "u723490217_Astra",
+      host = "185.224.138.133"
+    )
+  }
+
   return(con)
 }
 
