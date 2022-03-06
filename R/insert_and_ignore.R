24d78b467476c8b361028f2b25bd32ca4f7b8217
Ricardo Landolt <ricardo.landolt@stud.hslu.ch>
2022-03-06 19:41:45 +0100

    -add function for insert into postgres database -deleted unneeded function

---
+++ R/insert_and_ignore.R
@@ -0,0 +1,23 @@
+#' Writes a dataframe to the database
+#'
+#' This function allows you to write the data of dataframe to the database.
+#' @param con a connection to the postgres database
+#' @param df A dataframe
+#' @param tablename A string which contains the name of table in the database
+#' @param primarykeycols A vector which contains the names of the Primarykeys
+#' @export
+
+insert_and_ignore <- function(con, tablename,primarykeycols,df) {
+  # wipe the stage clean
+  rs <- DBI::dbSendQuery(con,glue::glue("delete from {tablename}_stage;"))
+  DBI::dbClearResult(rs)
+  # insert data to stage
+  rs <- DBI::dbAppendTable(con, glue::glue("{tablename}_stage"), df)
+  # # flip the stage over into final
+  rs <- DBI::dbSendQuery(con,glue::glue("insert into {tablename} select * from {tablename}_stage ON CONFLICT ({pk}) DO NOTHING;",
+                                        pk = glue_collapse(primarykeycols,",")))
+  DBI::dbClearResult(rs)
+}
+
+
+
--- R/scroll_into_view.R
+++ R/scroll_into_view.R
@@ -1,6 +1,6 @@
-#' Scroll a webelemt into the view 
+#' Scroll a webelemt into the view
 #'
-#' This function allows you open the browser with a specific url and sets the  
+#' This function allows you open the browser with a specific url and sets the
 #' Pageload/Implicit timeout. There is a option that cookie will be clicked.
 #' @param rD A Remote Driver Connection to the browser in docker container
 #' @param webel A Selenium Webelement
@@ -9,10 +9,11 @@
 #' url <- "https://www.hyundai.ch/"
 #' remDr <-  prepare_browser_function(url,"id","onetrust-reject-all-handler", containerport = 4445L)
 #' webelement <- remDr$findElement(using = "class", "menu")
-#' scroll_webel_intoview(remDr, webelement)
+#' scroll_into_view(remDr, webelement)
 
-scroll_webel_intoview <- function(rD, webel){
+scroll_into_view <- function(rD, webel){
   script <- "var viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);var elementTop = arguments[0].getBoundingClientRect().top;window.scrollBy(0, elementTop-(viewPortHeight/2));"
   rD$executeScript(script, args = list(webel))
+  # script <- "arguments[0].scrollIntoView(true);"
+  # rd$executeScript(script, args = list(nodes))
 }
-get
