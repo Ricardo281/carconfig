39d3deb0804740212f79d3012705a24423f3f49a
Ricardo Landolt <ricardo.landolt@stud.hslu.ch>
2022-03-09 13:43:27 +0100

    changed scroll function

--- R/scroll_into_view.R
+++ R/scroll_into_view.R
@@ -6,8 +6,8 @@
 #' @export
 
 scroll_into_view <- function(rd, webel){
-  script <- "arguments[0].scrollIntoView(true);"
-  rd$executeScript(script, args = list(webel))
-  # script <- "var viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);var elementTop = arguments[0].getBoundingClientRect().top;window.scrollBy(0, elementTop-(viewPortHeight/2));"
+  # script <- "arguments[0].scrollIntoView(true);"
   # rd$executeScript(script, args = list(webel))
+  script <- "var viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);var elementTop = arguments[0].getBoundingClientRect().top;window.scrollBy(0, elementTop-(viewPortHeight/2));"
+  rd$executeScript(script, args = list(webel))
 }
