d77c2a94e9e7f0d160a541e6bb723e425e24effd
Ricardo Landolt <ricardo.landolt@stud.hslu.ch>
2022-03-09 13:22:20 +0100

    changed scroll function

--- R/scroll_into_view.R
+++ R/scroll_into_view.R
@@ -1,13 +1,13 @@
 #' Scroll a webelemt into the view
 #'
 #' This function allows you to scroll webelements into the view
-#' @param rD A Remote Driver Connection to the browser in docker container
+#' @param rd A Remote Driver Connection to the browser in docker container
 #' @param webel A Selenium Webelement
 #' @export
 
-scroll_into_view <- function(rD, webel){
+scroll_into_view <- function(rd, webel){
   script <- "arguments[0].scrollIntoView(true);"
   rd$executeScript(script, args = list(webel))
   # script <- "var viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);var elementTop = arguments[0].getBoundingClientRect().top;window.scrollBy(0, elementTop-(viewPortHeight/2));"
-  # rD$executeScript(script, args = list(webel))
+  # rd$executeScript(script, args = list(webel))
 }
